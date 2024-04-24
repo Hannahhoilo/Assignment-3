285a49f626669f28b19119c2ccb14a5b1b1f2da5
Hannah Høilo <143997275+Hannahhoilo@users.noreply.github.com>
2024-04-24 23:48:08 +0200

    added delete button

--- assignment-3/src/App.jsx
+++ assignment-3/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import "./App.css";
-import ExpenceComponent from "./components/ExpenceComponent/ExpenceComponent"
-import styles from "./App.module.css"
+import ExpenceComponent from "./components/ExpenceComponent/ExpenceComponent";
+import styles from "./App.module.css";
+import { DeleteButton } from "./components/DeleteButton";
 
 function App() {
   return (
     <>
-    <div className={styles.main_container}>
-      <h1>Expense Tracker!</h1>
-      <ExpenceComponent></ExpenceComponent>
+      <div className={styles.main_container}>
+        <h1>Expense Tracker!</h1>
+        <ExpenceComponent></ExpenceComponent>
+        <DeleteButton></DeleteButton>
       </div>
     </>
   );
